d2e5391b31eabd5bb321f3c5d05fbc96607acc7c
Homey123-Chut <homey.chut@student.cadt.edu.kh>
2026-02-14 13:59:43 +0700

    bla-001

--- lib/services/rides_service.dart
+++ lib/services/rides_service.dart
@@ -8,6 +8,48 @@ import '../model/ride/ride.dart';
 ///
 class RidesService {
   static List<Ride> allRides = fakeRides;
+    static List<Ride> filterByDeparture(Location departure) {
+  List<Ride> filteredRides = [];
+
+  for (Ride ride in allRides) {
+    if (ride.departureLocation == departure) {
+      filteredRides.add(ride);
+    }
+  }
+
+  return filteredRides;
+}
+
+
+  static List<Ride> filterBySeatRequested(int seatRequested) {
+  List<Ride> filteredRides = [];
+
+  for (Ride ride in allRides) {
+    if (ride.availableSeats >= seatRequested) {
+      filteredRides.add(ride);
+    }
+  }
+
+  return filteredRides;
+}
+
+
+  static List<Ride> filterBy({
+  Location? departure,
+  int? seatRequested,
+}) {
+  List<Ride> result = [];
+
+  for (Ride ride in allRides) {
+    if ((departure == null || ride.departureLocation == departure) &&
+        (seatRequested == null || ride.availableSeats >= seatRequested)) {
+      result.add(ride);
+    }
+  }
+
+  return result;
+}
+
+}
 
  
-}
--- test/test_rides.dart
+++ test/test_rides.dart
@@ -5,5 +5,25 @@ import 'package:week_3_blabla_project/services/rides_service.dart';
 
 
 void main() {
+  final results = RidesService.filterBy(
+    departure: Location(
+      name: "Dijon",
+      country: Country.france
+      ),
+      seatRequested: 2,
+  );
+
+  final results1 = RidesService.filterBy(
+    departure: Location(
+      name: "London",
+      country: Country.uk,
+    ),
+    seatRequested: 2,
+  );
   
+  print("Number of rides found: ${results.length}");
+
+  for (var ride in results) {
+    print(ride);
+  }
 }
